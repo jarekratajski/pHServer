ee846181187f00d66f19d26471cc97ffc68a92e5
kcassia <kcassia@nate.com>
2016-03-28 14:09:53 +0900

    gui

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,8 @@
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
 
 /**
  * Created by Gye Hyeon Park on 2016-03-26.
@@ -12,21 +10,100 @@ import java.net.Socket;
 
 public class Server
 {
+    private static ConnectionThread thread;
     public static void main(String[] args)
     {
-        ServerSocket server = null;
+        JFrame frame = new JFrame("pH Server");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize.width/4, screenSize.height/4);
+        frame.setResizable(false);
+        Dimension frameSize = frame.getSize();
+        frame.setLocation((screenSize.width - frameSize.width)/2, (screenSize.height - frameSize.height)/2);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2, 2));
+        Font font = new Font("Sans Serif", Font.ITALIC, 40);
+
+        JLabel ipLabel = new JLabel("IP");
+        ipLabel.setBackground(Color.CYAN);
+        ipLabel.setOpaque(true);
+        ipLabel.setFont(font);
+        ipLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(ipLabel);
+
+        JTextField ipText = null;
+        try{ipText = new JTextField(InetAddress.getLocalHost().getHostAddress()); }
+        catch(UnknownHostException ignored){}
+        ipText.setFont(font);
+        ipText.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(ipText);
+
+        JLabel portLabel = new JLabel("Port");
+        portLabel.setBackground(Color.RED);
+        portLabel.setOpaque(true);
+        portLabel.setFont(font);
+        portLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(portLabel);
+
+        JTextField portText = new JTextField("7777");
+        portText.setFont(font);
+        portText.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(portText);
+
+        frame.add(panel);
+        JButton button = new JButton("Connect");
+        button.setFont(font);
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                if(button.getText().equals("Connect"))
+                {
+                    thread = new ConnectionThread(Integer.parseInt(portText.getText()));
+                    thread.start();
+                    button.setText("Disconnect");
+                }
+                else
+                {
+                    thread.kill();
+                    button.setText("Connect");
+                }
+            }
+        });
+        frame.add(button, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
+}
+
+class ConnectionThread extends Thread
+{
+    private ServerSocket server;
+    private BufferedReader reader;
+
+    public ConnectionThread(int portNum)
+    {
+        try{server = new ServerSocket(portNum);}
+        catch(IOException ignored){}
+    }
+
+    @Override
+    public void run()
+    {
         try
         {
-            server = new ServerSocket(7777);
             Socket socket = server.accept();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             while(true)
             {
                 String msg = reader.readLine();
                 System.out.println(msg);
                 Robot robot = new Robot();
                 if(msg == null)
+                {
+                    System.out.println("null");
                     break;
+                }
                 switch(msg)
                 {
                     case "F5":
@@ -46,7 +123,18 @@ public class Server
         finally
         {
             try{server.close();}
-            catch(IOException ignored){}
+            catch(Exception ignored){}
         }
+    }
+
+    public void kill()
+    {
+        try
+        {
+            reader = null;
+//            reader.close();
+            server.close();
+        }
+        catch(Exception ignored){}
     }
 }
